f2860651298858dd5b247376184318f176350c13
hellisart <7info7web@gmail.com>
2023-03-10 15:30:43 +0600

    test vercel deploy

--- src/_app.ts
+++ src/_app.ts
@@ -1,19 +1,19 @@
 import type { App } from 'vue';
 import VueDirectus from 'vue-directus';
-// import VueApollo from 'vue-apollo'
-// import ApolloClient from 'apollo-boost'
+import VueApollo from 'vue-apollo'
+import ApolloClient from 'apollo-boost'
 
-//
-// const apolloClient = new ApolloClient({
-//     uri: 'http://0.0.0.0:8055/graphql'
-// })
-//
-// const apolloProvider = new VueApollo({
-//     defaultClient: apolloClient,
-// })
+
+const apolloClient = new ApolloClient({
+    uri: 'http://0.0.0.0:8055/graphql'
+})
+
+const apolloProvider = new VueApollo({
+    defaultClient: apolloClient,
+})
 
 export default (app: App) => {
-    // app.use(apolloProvider),
+    app.use(apolloProvider),
     app.use(VueDirectus, {
         apiUrl: 'http://0.0.0.0:8055/',
         auth: "storage",
