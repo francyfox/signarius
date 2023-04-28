ee0e5f23c71fb14729c5546c5be98f0137c01770
hellisart <7info7web@gmail.com>
2023-04-28 11:00:27 +0600

    fix tippy css error on vercel 4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from 'astro/config';
+import {defineConfig} from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import vue from "@astrojs/vue";
@@ -7,17 +7,18 @@ import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
-  integrations: [
-    mdx(),
-    sitemap(),
-    vue({ appEntrypoint: '/src/_app' })
-  ],
-  vite: {
-    css: {
-      devSourcemap: true
-    }
-  },
-  output: "server",
-  adapter: vercel({})
+    site: 'https://example.com',
+    integrations: [
+        mdx(),
+        sitemap(),
+        vue({appEntrypoint: '/src/_app'})
+    ],
+    vite: {
+        css: {
+            devSourcemap: true,
+            noExternal: ['tippy.js'],
+        }
+    },
+    output: "server",
+    adapter: vercel({})
 });
