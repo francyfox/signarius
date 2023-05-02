283200f164b7cee57be3948d6e9259aec7eedd13
hellisart <7info7web@gmail.com>
2023-05-02 15:16:18 +0600

    moved console to npm package

--- console/command.ts
+++
@@ -1,9 +0,0 @@
-import FormkitSchemaGenerator from "./module/formkit/formkit.schema-generator";
-
-async function start() {
-  const generator = new FormkitSchemaGenerator(['post', 'textblock', 'directus_users']);
-  const FormKitSchema = await generator.saveFile()
-}
-
-
-await start()
